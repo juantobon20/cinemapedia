e92e962c857e987fe3961ced63b282ca2bcf4c64
juan.tobon <juan.tobon@syscafe.com>
2024-02-08 18:21:37 -0500

    Pasar de una screen a otra con parametros go_router

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -2,6 +2,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/config/helpers/human_format.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MovieHorizontalListView extends StatefulWidget {
 
@@ -102,7 +103,10 @@ class _Slide extends StatelessWidget {
                   if (loadingProgress != null) {
                     return const Center(child: CircularProgressIndicator(strokeWidth: 2));
                   }
-                  return FadeIn(child: child);
+                  return GestureDetector(
+                    onTap: () => context.push('/movie/${movie.id}'),
+                    child: FadeIn(child: child)
+                  );
                 },
               ),
             ),
--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,4 +1,4 @@
-export 'package:cinemapedia/presentation/screens/movies/movie_slide_show.dart';
+export 'package:cinemapedia/presentation/widgets/movies/movie_slide_show.dart';
 export 'shared/custom_app_bar.dart';
 export 'shared/custom_bottom_navigation_bar.dart';
 export 'movies/movie_horizontal_listview.dart';
