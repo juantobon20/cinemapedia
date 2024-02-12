0b57d3440f758eb00d706c95e5fcce39dd78b688
juan.tobon <juan.tobon@syscafe.com>
2024-02-12 18:57:44 -0500

    Go router official implement

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,13 +1,48 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
+
+    ShellRoute(
+      builder:(context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) {
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder :(context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+
+                return MovieScreen(movieId: movieId);
+              }
+            ),
+          ]
+        ),
+
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        ),
+      ]
+    )
+    /*GoRoute(
       path: '/',
       name: HomeScreen.name,
-      builder :(context, state) => const HomeScreen(),
+      builder :(context, state) => const HomeScreen(
+        childView: HomeView(),
+      ),
       routes: [
         GoRoute(
           path: 'movie/:id',
@@ -19,6 +54,6 @@ final appRouter = GoRouter(
           }
         ),
       ]
-    )
+    )*/
   ]
 );
